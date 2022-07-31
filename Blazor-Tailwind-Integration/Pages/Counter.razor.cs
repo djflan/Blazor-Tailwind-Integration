9df36e50a6247a0f101170dcbd3bf0e04fe310b3
Dan Flanigan <djflan@gmail.com>
2022-07-30 20:55:18 -0500

    Added counter modification per MS example

---
+++ Blazor-Tailwind-Integration/Pages/Counter.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor_Tailwind_Integration.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int _currentCount = 0;
+
+        [Parameter]
+        public int IncrementAmount { get; set; } = 1;
+
+        private void IncrementCount()
+        {
+            _currentCount+= IncrementAmount;
+        }
+    }
+}
